7a9d8cce851bc1f5e137523318d2d086b61ca76c
LucasLemuel13 <lucaslemuel13@live.com>
2024-02-20 20:37:58 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,44 @@
+alert('Boas vindas ao jogo do número secreto!');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1); 
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1; 
+
+//enquanto chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // se chute for igual ao número secreto
+   if (chute == numeroSecreto) {
+    break;
+       } else {
+        if (chute > numeroSecreto ) {
+            alert(` o número secreto é menor que ${chute}`);
+        } else {
+            alert(` o número secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++;
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! voê descobriu o número secreto que é ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+//if (tentativas > 1) {
+//    alert(`Isso ai! voê descobriu o número secreto que é ${numeroSecreto} com ${tentativas} tentativas.`);
+//} else {
+//    alert(`Isso ai! voê descobriu o número secreto que é ${numeroSecreto} com ${tentativas} tentativa.`);
+//}
+
+
+
+
+
+
+
+
+
+
+
+
